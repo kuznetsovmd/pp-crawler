e54baabdb1968c5003c08deaddcb223887a3a625
kuznetsovmd <kuznetsov.m.d@yandex.ru>
2025-08-03 18:45:58 +0300

    Separated pool functions in a file

---
+++ src/pp_crawler/core/pool.py
@@ -0,0 +1,61 @@
+import atexit
+import logging
+import signal
+import sys
+
+from pp_crawler.core.config import DriverConfig
+from pp_crawler.core.functions import get_logger
+from pp_crawler.crawler.web.driver import Driver
+
+
+class ExitFilter(logging.Filter):
+    def filter(self, record):
+        if not record.exc_info:
+            return True
+        _, exc_value, _ = record.exc_info
+        return not isinstance(exc_value, (SystemExit, BrokenPipeError))
+
+
+def sysexit(*_):
+    logger = get_logger()
+    logger.info("Closing worker")
+    raise SystemExit(0)
+
+
+def init_logger(queue):
+    root = logging.getLogger()
+    root.setLevel(logging.INFO)
+    for handler in root.handlers[:]:
+        root.removeHandler(handler)
+    h = logging.handlers.QueueHandler(queue)
+    root.addHandler(h)
+
+
+def worker_constructor(queue, config: DriverConfig):
+    signal.signal(signal.SIGINT, signal.SIG_IGN)
+    signal.signal(signal.SIGTERM, sysexit)
+    atexit.register(worker_destructor)
+    init_logger(queue)
+    Driver.set_config(config)
+
+
+def worker_destructor():
+    Driver.close()
+
+
+def logger_initializer(queue):
+    signal.signal(signal.SIGINT, signal.SIG_IGN)
+    f = logging.Formatter(
+        "%(asctime)s - [%(name)s] %(levelname)s: %(message)s", "%H:%M:%S"
+    )
+    h = logging.StreamHandler(sys.stdout)
+    h.addFilter(ExitFilter())
+    h.setLevel(logging.INFO)
+    h.setFormatter(f)
+    logging.getLogger().addHandler(h)
+    while True:
+        record = queue.get()
+        if record is None:
+            break
+        logger = logging.getLogger(record.name)
+        logger.handle(record)
